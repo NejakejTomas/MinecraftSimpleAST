d03049eee8dedcc80626c46b7b3d503902cbf2c4
Tomáš Kiec <kiectoma@fit.cvut.cz>
2020-08-23 10:31:34 +0200

    Added StyledTextNode

--- src/main/kotlin/com/discord/core/node/StyledTextNode.kt
+++ src/main/kotlin/com/discord/core/node/StyledTextNode.kt
@@ -1,7 +1,3 @@
-/**
- * Edited 2020 UniGa
- */
-
 package com.discord.core.node
 
 import net.minecraft.text.LiteralText
@@ -11,16 +7,16 @@ import net.minecraft.text.Style
  * @param T Type of Span to apply
  * @param F The format context, can be any object that holds what's required for formatting. See [format].
  */
-open class StyleNode<F, T>(protected val style: Style) : Node<F>() {
+open class StyledTextNode<F, T>(protected var style : Style?, protected var text : String?) : Node<F>() {
 
-  override fun toString() = "${javaClass.simpleName} >\n" +
-      getChildren()?.joinToString("\n->", prefix = ">>", postfix = "\n>|") {
-        it.toString()
-      }
+    override fun toString() = "${javaClass.simpleName} >\n" +
+            getChildren()?.joinToString("\n->", prefix = ">>", postfix = "\n>|") {
+                it.toString()
+            }
 
     override fun format(renderContext: F): LiteralText {
-        val text = LiteralText("")
-        text.style = style
+        val text = LiteralText(text ?: "")
+        if (style != null) text.style = style
 
         getChildren()?.forEach {
             text.append(it.format(renderContext))
